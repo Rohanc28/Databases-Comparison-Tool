467d9a9424607f358d1b2661c3941e015193659b
Rohanc28 <rochaturvedi01@gmail.com>
2023-04-15 13:53:22 +0530

    Alert after submit

--- UI/ServerConnect/ServerConnect_script.js
+++ UI/ServerConnect/ServerConnect_script.js
@@ -18,4 +18,33 @@ document.addEventListener('DOMContentLoaded', () => {
       toggleBtn.innerHTML = 'Dark Mode';
     }
   });
-})
+  
+  // get the form element
+  const form = document.getElementById('server-form');
+  
+ // add a submit event listener to the form
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    // get the input fields
+    const serverName = document.getElementById('serverName').value;
+    const databaseName = document.getElementById('databaseName').value;
+    const username = document.getElementById('userName').value;
+    const password = document.getElementById('password').value;
+    
+    
+    // check if both database1 and database2 are filled
+    if (username && password && databaseName && serverName) {
+      alert(`Inputs: \n${serverName} \n${databaseName} \n${username} \n${password}`);
+      console.log("CODE AFTER ALERT ENDS");
+    }
+     // check if only database1 is filled
+    else {
+      alert(`Multiple invalid field inputs`);
+      console.log("CODE AFTER ALERT ENDS");
+    }
+    //if both blank
+    
+
+  });
+});
