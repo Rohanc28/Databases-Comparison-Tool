1ab94f969ffeecb0c8b4c878d361de9aaaac2b27
Rohanc28 <rochaturvedi01@gmail.com>
2023-04-17 13:00:24 +0530

    Added support for xlsx

--- UI/TableView/TableView_script.js
+++ UI/TableView/TableView_script.js
@@ -1,4 +1,4 @@
-console.log("new")
+
 document.addEventListener('DOMContentLoaded', () => {
   const toggleBtn = document.querySelector('#toggleBtn');
   const body = document.querySelector('body');
@@ -30,26 +30,39 @@ document.addEventListener('DOMContentLoaded', () => {
 // 
 
 
+
 // Select the file input element
-const fileInput = document.getElementById('csv-file1');
+const fileInput = document.getElementById('file-input1');
 
 // When the user selects a file
 fileInput.addEventListener('change', () => {
   // Get the selected file
   const file = fileInput.files[0];
 
-  // Check if the file is a CSV file
-  if (!file || file.type !== 'text/csv') {
-    alert('Please select a CSV file.');
+  // Check if the file is a CSV or Excel file
+  if (!file || (file.type !== 'text/csv' && file.type !== 'application/vnd.ms-excel' && file.type !== 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')) {
+    alert('Please select a CSV or Excel file.');
     return;
   }
 
-  // Call the loadCSV function to parse the file and create the table
-  loadCSV1(file);
+  // Call the appropriate function to parse the file and create the table
+  if (file.type === 'text/csv') {
+    loadCSV(file);
+  } else if (file.type === 'application/vnd.ms-excel' || file.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+    loadExcel(file);
+  } else {
+    alert('Unsupported file type.');
+  }
 });
 
-// Function to parse CSV data and create an HTML table
-function loadCSV1(file) {
+
+
+// CSV FILE
+
+
+
+function loadCSV(file) {
+  console.log("CALLED CSV");
   // Create a new FileReader
   const reader = new FileReader();
 
@@ -113,6 +126,79 @@ function loadCSV1(file) {
 
 
 
+
+
+// EXCEL FILE
+
+
+// Function to parse Excel data and create an HTML table
+function loadExcel(file) {
+  console.log("CALLED EXCEL"); //is being called
+  
+  // Create a new FileReader
+  const reader = new FileReader();
+
+  // When the FileReader finishes loading the file
+  reader.addEventListener('load', () => {
+    // Parse the Excel data using SheetJS library
+    const workbook = XLSX.read(reader.result, { type: 'binary' });
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+    const rows = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+    // Create the HTML table
+    const table = document.createElement('table');
+    table.classList.add('table', 'table-striped', 'bg-light');
+
+    // Create the table header
+    const thead = document.createElement('thead');
+    thead.classList.add('thead-dark');
+    const headerRow = document.createElement('tr');
+    rows[0].forEach(header => {
+      const th = document.createElement('th');
+      th.textContent = header;
+      headerRow.appendChild(th);
+    });
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+
+    // Create the table body
+    const tbody = document.createElement('tbody');
+    tbody.setAttribute('id', 'table-row');
+    rows.slice(1).forEach(rowData => {
+      const row = document.createElement('tr');
+      rowData.forEach(cellData => {
+        const cell = document.createElement('td');
+        cell.textContent = cellData;
+        row.appendChild(cell);
+      });
+      tbody.appendChild(row);
+    });
+    table.appendChild(tbody);
+
+    // Replace the existing content with the new table
+    const contentDiv = document.getElementById('content1');
+    contentDiv.innerHTML = '';
+    contentDiv.classList.remove('d-none');
+    contentDiv.appendChild(table);
+  });
+
+  // Handle errors during file reading
+  reader.addEventListener('error', () => {
+    alert('Error during reading the file. Please try again.');
+  });
+
+  // Read the file as binary string
+  try {
+    reader.readAsBinaryString(file);
+  } catch (err) {
+    alert('Error reading the file. Please try again.');
+  }
+}
+
+
+
+
 //content 2
 
 
@@ -122,82 +208,170 @@ function loadCSV1(file) {
 
 
 // Select the file input element
-const fileInput2 = document.getElementById('csv-file2');
+const fileInput2 = document.getElementById('file-input2');
 
 // When the user selects a file
 fileInput2.addEventListener('change', () => {
   // Get the selected file
-  const file2 = fileInput2.files[0];
+  const file = fileInput2.files[0];
 
-  // Check if the file is a CSV file
-  if (!file2 || file2.type !== 'text/csv') {
-    alert('Please select a CSV file.');
+  // Check if the file is a CSV or Excel file
+  if (!file || (file.type !== 'text/csv' && file.type !== 'application/vnd.ms-excel' && file.type !== 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')) {
+    alert('Please select a CSV or Excel file.');
     return;
   }
 
-  // Call the loadCSV function to parse the file and create the table
-  loadCSV2(file2);
+  // Call the appropriate function to parse the file and create the table
+  if (file.type === 'text/csv') {
+    loadCSV2(file);
+  } else if (file.type === 'application/vnd.ms-excel' || file.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+    loadExcel2(file);
+  } else {
+    alert('Unsupported file type.');
+  }
 });
 
-// Function to parse CSV data and create an HTML table
-function loadCSV2(file2) {
-  console.log("CALLED 2") //is being called
+
+
+
+
+
+
+// CSV FILE
+
+
+
+function loadCSV2(file) {
+  console.log("CALLED CSV");
   // Create a new FileReader
-  const reader2 = new FileReader();
+  const reader = new FileReader();
 
   // When the FileReader finishes loading the file
-  reader2.addEventListener('load', () => {
+  reader.addEventListener('load', () => {
     // Parse the CSV data
-    const rows2 = reader2.result.split('\n').map(row => row.split(','));
+    const rows = reader.result.split('\n').map(row => row.split(','));
 
     // Create the HTML table
-    const table2 = document.createElement('table');
-    table2.classList.add('table', 'table-striped','bg-light');
+    const table = document.createElement('table');
+    table.classList.add('table', 'table-striped','bg-light');
     
     
     // Create the table header
-    const thead2 = document.createElement('thead');
-    thead2.classList.add('thead-dark');
-    const headerRow2 = document.createElement('tr');
-    rows2[0].forEach(header => {
-      const th2 = document.createElement('th');
-      th2.textContent = header;
-      headerRow2.appendChild(th2);
+    const thead = document.createElement('thead');
+    thead.classList.add('thead-dark');
+    const headerRow = document.createElement('tr');
+    rows[0].forEach(header => {
+      const th = document.createElement('th');
+      th.textContent = header;
+      headerRow.appendChild(th);
     });
-    thead2.appendChild(headerRow2);
-    table2.appendChild(thead2);
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
 
     // Create the table body
-    const tbody2 = document.createElement('tbody');
-    tbody2.setAttribute('id','table-row-2');
-    rows2.slice(1).forEach(rowData => {
-      const row2 = document.createElement('tr');
+    const tbody = document.createElement('tbody');
+    tbody.setAttribute('id','table-row');
+    rows.slice(1).forEach(rowData => {
+      const row = document.createElement('tr');
       rowData.forEach(cellData => {
-        const cell2 = document.createElement('td');
-        cell2.textContent = cellData;
-        row2.appendChild(cell2);
+        const cell = document.createElement('td');
+        cell.textContent = cellData;
+        row.appendChild(cell);
       });
-      tbody2.appendChild(row2);
+      tbody.appendChild(row);
     });
-    table2.appendChild(tbody2);
+    table.appendChild(tbody);
 
     // Replace the existing content with the new table
-    const contentDiv2 = document.getElementById('content2');
-    contentDiv2.innerHTML = ''; 
-    contentDiv2.classList.remove('d-none');
-    contentDiv2.appendChild(table2);
-    console.log(reader2.result);
+    const contentDiv = document.getElementById('content2');
+    contentDiv.innerHTML = ''; 
+    contentDiv.classList.remove('d-none');
+    contentDiv.appendChild(table);
+    console.log(reader.result);
   });
 
   // Handle errors during file reading
-  reader2.addEventListener('error', () => {
-    alert('Error during reading the file. Please try again.');
+  reader.addEventListener('error', () => {
+    alert('Error reading the file. Please try again.');
   });
 
   // Read the file as text
   try {
-    reader2.readAsText(file2);
+    reader.readAsText(file);
   } catch (err) {
-    alert('Cant read as text, Error reading the file. Please try again.');
+    alert('Error reading the file. Please try again.');
+  }
+}
+
+
+
+
+
+
+// EXCEL FILE
+
+
+// Function to parse Excel data and create an HTML table
+function loadExcel2(file) {
+  console.log("CALLED EXCEL"); //is being called
+  
+  // Create a new FileReader
+  const reader = new FileReader();
+
+  // When the FileReader finishes loading the file
+  reader.addEventListener('load', () => {
+    // Parse the Excel data using SheetJS library
+    const workbook = XLSX.read(reader.result, { type: 'binary' });
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+    const rows = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+    // Create the HTML table
+    const table = document.createElement('table');
+    table.classList.add('table', 'table-striped', 'bg-light');
+
+    // Create the table header
+    const thead = document.createElement('thead');
+    thead.classList.add('thead-dark');
+    const headerRow = document.createElement('tr');
+    rows[0].forEach(header => {
+      const th = document.createElement('th');
+      th.textContent = header;
+      headerRow.appendChild(th);
+    });
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+
+    // Create the table body
+    const tbody = document.createElement('tbody');
+    tbody.setAttribute('id', 'table-row');
+    rows.slice(1).forEach(rowData => {
+      const row = document.createElement('tr');
+      rowData.forEach(cellData => {
+        const cell = document.createElement('td');
+        cell.textContent = cellData;
+        row.appendChild(cell);
+      });
+      tbody.appendChild(row);
+    });
+    table.appendChild(tbody);
+
+    // Replace the existing content with the new table
+    const contentDiv = document.getElementById('content2');
+    contentDiv.innerHTML = '';
+    contentDiv.classList.remove('d-none');
+    contentDiv.appendChild(table);
+  });
+
+  // Handle errors during file reading
+  reader.addEventListener('error', () => {
+    alert('Error during reading the file. Please try again.');
+  });
+
+  // Read the file as binary string
+  try {
+    reader.readAsBinaryString(file);
+  } catch (err) {
+    alert('Error reading the file. Please try again.');
   }
 }
