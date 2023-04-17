e7c6b02ea1f53782c3b2e890af9ef316cb5f1532
Rohanc28 <rochaturvedi01@gmail.com>
2023-04-17 12:12:48 +0530

    Added second tableView

--- UI/TableView/TableView_script.js
+++ UI/TableView/TableView_script.js
@@ -27,19 +27,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 //
 // /
-// /
-// //
-// /
-
-// /
-//
-//
-//
-//
+// 
 
 
 // Select the file input element
-const fileInput = document.getElementById('csv-file');
+const fileInput = document.getElementById('csv-file1');
 
 // When the user selects a file
 fileInput.addEventListener('change', () => {
@@ -53,11 +45,11 @@ fileInput.addEventListener('change', () => {
   }
 
   // Call the loadCSV function to parse the file and create the table
-  loadCSV(file);
+  loadCSV1(file);
 });
 
 // Function to parse CSV data and create an HTML table
-function loadCSV(file) {
+function loadCSV1(file) {
   // Create a new FileReader
   const reader = new FileReader();
 
@@ -70,7 +62,7 @@ function loadCSV(file) {
     const table = document.createElement('table');
     table.classList.add('table', 'table-striped','bg-light');
     
-
+    
     // Create the table header
     const thead = document.createElement('thead');
     thead.classList.add('thead-dark');
@@ -98,10 +90,11 @@ function loadCSV(file) {
     table.appendChild(tbody);
 
     // Replace the existing content with the new table
-    const contentDiv = document.getElementById('content');
+    const contentDiv = document.getElementById('content1');
     contentDiv.innerHTML = ''; 
     contentDiv.classList.remove('d-none');
     contentDiv.appendChild(table);
+    console.log(reader.result);
   });
 
   // Handle errors during file reading
@@ -114,5 +107,97 @@ function loadCSV(file) {
     reader.readAsText(file);
   } catch (err) {
     alert('Error reading the file. Please try again.');
+  }
+}
+
+
+
+
+//content 2
+
+
+//
+// /
+// 
+
+
+// Select the file input element
+const fileInput2 = document.getElementById('csv-file2');
+
+// When the user selects a file
+fileInput2.addEventListener('change', () => {
+  // Get the selected file
+  const file2 = fileInput2.files[0];
+
+  // Check if the file is a CSV file
+  if (!file2 || file2.type !== 'text/csv') {
+    alert('Please select a CSV file.');
+    return;
+  }
+
+  // Call the loadCSV function to parse the file and create the table
+  loadCSV2(file2);
+});
+
+// Function to parse CSV data and create an HTML table
+function loadCSV2(file2) {
+  console.log("CALLED 2") //is being called
+  // Create a new FileReader
+  const reader2 = new FileReader();
+
+  // When the FileReader finishes loading the file
+  reader2.addEventListener('load', () => {
+    // Parse the CSV data
+    const rows2 = reader2.result.split('\n').map(row => row.split(','));
+
+    // Create the HTML table
+    const table2 = document.createElement('table');
+    table2.classList.add('table', 'table-striped','bg-light');
+    
+    
+    // Create the table header
+    const thead2 = document.createElement('thead');
+    thead2.classList.add('thead-dark');
+    const headerRow2 = document.createElement('tr');
+    rows2[0].forEach(header => {
+      const th2 = document.createElement('th');
+      th2.textContent = header;
+      headerRow2.appendChild(th2);
+    });
+    thead2.appendChild(headerRow2);
+    table2.appendChild(thead2);
+
+    // Create the table body
+    const tbody2 = document.createElement('tbody');
+    tbody2.setAttribute('id','table-row-2');
+    rows2.slice(1).forEach(rowData => {
+      const row2 = document.createElement('tr');
+      rowData.forEach(cellData => {
+        const cell2 = document.createElement('td');
+        cell2.textContent = cellData;
+        row2.appendChild(cell2);
+      });
+      tbody2.appendChild(row2);
+    });
+    table2.appendChild(tbody2);
+
+    // Replace the existing content with the new table
+    const contentDiv2 = document.getElementById('content2');
+    contentDiv2.innerHTML = ''; 
+    contentDiv2.classList.remove('d-none');
+    contentDiv2.appendChild(table2);
+    console.log(reader2.result);
+  });
+
+  // Handle errors during file reading
+  reader2.addEventListener('error', () => {
+    alert('Error during reading the file. Please try again.');
+  });
+
+  // Read the file as text
+  try {
+    reader2.readAsText(file2);
+  } catch (err) {
+    alert('Cant read as text, Error reading the file. Please try again.');
   }
 }
